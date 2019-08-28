2781020627ecb3a760951a1b98eaac8de626b4da
vadzim84 <vkaraniec@gmail.com>
2019-08-28 15:10:44 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+    <header className= "header">
+        <img src= 'https://3dwarehouse.sketchup.com/warehouse/v1.0/publiccontent/e5404b92-8ae7-4cca-8dd3-c7dac07f12b1'/>
+    </header>
+    )
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className='content'>
+      <div>
+        <img src='https://tdaglobalcycling.com/wp-content/uploads/2018/03/17_259_Earth_Hour_Social_Web_1600x600_v2.jpeg' />
+      </div>
+      <div>
+        avatar + description
+      </div>
+      <div>
+        My posts
+        <div>
+          New posts
+        </div>
+        <div>
+          <div>
+            Post1
+          </div>
+          <div>
+            Post2
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Profile;
